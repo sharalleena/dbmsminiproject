f6ad6938ee214b973267adc213c055d7168b2c12
sharalleena <70796659+sharalleena@users.noreply.github.com>
2020-12-26 16:06:08 +0530

    Update and rename register.html to register.php

--- register.php
+++ register.php
@@ -5,10 +5,10 @@
         
     </head>
     <body>
-    <?php
-     include 'conn.php';
-     if(isset($_POST['submit']))
-    {
+<?php
+ include 'conn.php';
+ if(isset($_POST['submit']))
+ {
         $username=mysqli_real_escape_string($con, $_POST['username']) ;
         $email= mysqli_real_escape_string($con, $_POST['email']) ;
         $password= mysqli_real_escape_string($con, $_POST['password']) ;
@@ -22,49 +22,56 @@
         $emailcount = mysqli_num_rows($query);
         if($emailcount>0)
         {
-             echo "registration sucessful";
+             echo "email already exists";
         }
-         else
-       {
-       if($pass===$cpass)
-       {
-        $insertquery = "insert into ad_register(username,email,password,cpassword)
-        values('$username','$email','$pass',$cpass)";
-        $iquery = mysqli_query($con,$insertquery);
-        if($iquery)
-          {
-            ?>
-            <script>
-             alert(" inserted  sucessful");
-            </script>
-            <?php
-          }
+        $usernamequery = "select * from ad_register where usename ='$username'";
+        $userquery  = mysqli_query($con,$usernamequery);
+        $usernamecount = mysqli_num_rows($userquery);
+        if($usernamecount>0)
+        {
+             echo "username already taken";
+        }
         else
           {
-            ?>
-            <script>
-             alert("rinsertion not sucessful");
-            </script>
-            <?php
-          }
+               if($password === $cpassword)
+               {
+                $insertquery = "insert into ad_register(username,email,password,cpassword)
+                values('$username','$email','$pass','$cpass')";
+                $iquery = mysqli_query($con,$insertquery);
+                if($iquery)
+                   {
+                    ?>
+                    <script>
+                     alert(" inserted  sucessful");
+                    </script>
+                    <?php
+                   }
+                  else
+                   {
+                    ?>
+                    <script>
+                     alert("insertion not sucessful");
+                    </script>
+                    <?php
+                   }
+               }
+              else
+             { 
+              ?>
+              <script>
+               alert("password not match");
+              </script>
+              <?php
+             }      
        }
-       else
-       { 
-        ?>
-        <script>
-         alert("password not match");
-        </script>
-        <?php
-       }
-    }
-    
-    }
-
+            
+}
+        
 
 ?>        <div class="registration-page">
             <div class="form">
                 <p ><font size="+4" color="#29b10e" font-style="oblique">Sign Up</font> </p>
-                 <form class="register-form" method ="POST">
+                 <form class="register-form"action.="" method ="POST">
                     <input type="text" name="username" placeholder="USERNAME" required/>
                     <input type="email" name="email" placeholder="EMAIL ID" required/>
                     <input type="password" name="password" placeholder="PASSWORD" required/>
